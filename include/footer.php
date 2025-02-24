70136ba23b409dcec072e203cc75ca66c7a7586f
Khoa Tran <22010303@st.phenikaa-uni.edu.vn>
2025-02-25 00:41:42 +0700

    Initial commit

---
+++ include/footer.php
@@ -0,0 +1,67 @@
+
+	<!-- footer -->
+	<footer>
+		<div class="footer-top-first">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<!-- footer first section -->
+				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi</h2>
+				<p class="footer-main mb-4">
+				Nếu bạn đang cân nhắc mua một chiếc laptop mới, tìm kiếm một dàn âm thanh xe hơi mạnh mẽ hoặc mua một chiếc HDTV mới,
+				 chúng tôi giúp bạn dễ dàng tìm thấy những gì bạn cần với mức giá hợp lý.
+				  Chúng tôi cung cấp giá thấp hàng ngày cho các sản phẩm như TV,
+				   laptop, điện thoại di động, máy tính bảng và iPad, trò chơi điện tử,
+				    máy tính để bàn, máy ảnh và máy quay video, âm thanh, video và nhiều hơn nữa.</p>
+				<!-- //footer first section -->
+				<!-- footer second section -->
+				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Miễn phí vận chuyển</h3>
+								<p>Đơn hàng trên 100$</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Vận chuyển nhanh</h3>
+								<p>Toàn quốc</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Tin cậy</h3>
+								<p>Sự đảm bảo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		
+		<!-- //footer third section -->
+		
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© 2025 KK Store. All rights reserved | Design by Khoa Tran
+				<a href="http://facebook.com"> KKK.</a>
+			</p>
+		</div>
+	</div>
+	<!-- //copyright -->
